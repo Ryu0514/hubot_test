4ae39e733b08e72c3b4fda9acc3e8053343403e3
Ryu0514 <r514.ldps@gmail.com>
2017-05-19 13:29:35 +0900

    Create stgSwitch.sh

---
+++ scripts/shell/stgSwitch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+username = 'koishi0910'
+hostname = '10.213.201.2'
+passwdfile = 'passwd_toscana.dat'
+portnum = '22'
+command = $hostname:$portnum" /ssh /auth=password /user="$username
+
+ssh $command
+echo "execute shell script"
